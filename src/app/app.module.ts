65dc0fb5c714433fbcfb40af49c523956ea76019
jonyroww <jonyroww@gmail.com>
2020-07-02 13:40:09 +0300

    MailerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,25 @@ import { AuthModule } from '../auth/auth.module';
 import { SMSRuModule } from 'node-sms-ru/nestjs';
 import { ConfigService } from '../config/config.service';
 import { ConfigModule } from '../config/config.module';
+import path from 'path';
+import appRootPath from 'app-root-path';
+import { HandlebarsAdapter, MailerModule } from '@nest-modules/mailer';
 
 @Module({
   imports: [
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        defaults: {
+          from: configService.get('EMAIL_FROM'),
+        },
+        transport: configService.get('SMTP_URL'),
+        template: {
+          dir: path.join(appRootPath.toString(), 'templates'),
+          adapter: new HandlebarsAdapter(),
+        },
+      }),
+      inject: [ConfigService],
+    }),
     SMSRuModule.forRootAsync({
       useFactory: (config: ConfigService) => ({
         api_id: config.get('SMS_API_ID'),
