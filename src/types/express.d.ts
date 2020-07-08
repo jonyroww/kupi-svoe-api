8d025b884697dec948bea01cec80538190918bc8
Edgar <starguitar307@gmail.com>
2020-07-08 02:47:48 +0200

    Fix Express.Request.user type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,11 @@
-import { User } from '../../src/users/entities/User.entity';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { User as _User } from '../../src/users/entities/User.entity';
 
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    export interface User extends _User {}
+
     interface Request {
       user?: User;
     }
